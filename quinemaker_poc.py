1b654a4467c949123f80651f7895f5f960337e3e
yellowsubmarine1447 <48506404+yellowsubmarine1447@users.noreply.github.com>
2024-01-11 19:46:55 +1100

    Update quinemaker_poc.py

--- quinemaker_poc.py
+++ quinemaker_poc.py
@@ -1,1 +1,2 @@
-
+a = 1
+b="""a = 1""";print(b);a='b=y;print(b);a=x;print(a.replace(chr(121),chr(34)+chr(34)+chr(34)+b+chr(34)+chr(34)+chr(34)).replace(chr(120),chr(39)+a+chr(39)))';print(a.replace(chr(121),chr(34)+chr(34)+chr(34)+b+chr(34)+chr(34)+chr(34)).replace(chr(120),chr(39)+a+chr(39)))
