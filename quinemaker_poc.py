15d6c92a28fca725a6fead942078a11517b2c087
yellowsubmarine1447 <48506404+yellowsubmarine1447@users.noreply.github.com>
2024-01-11 20:53:11 +1100

    Update quinemaker_poc.py

--- quinemaker_poc.py
+++ quinemaker_poc.py
@@ -1,2 +1,2 @@
-a = 1
-b="""a = 1""";print(b);a='b=y;print(b);a=x;print(a.replace(chr(121),chr(34)+chr(34)+chr(34)+b+chr(34)+chr(34)+chr(34)).replace(chr(120),chr(39)+a+chr(39)))';print(a.replace(chr(121),chr(34)+chr(34)+chr(34)+b+chr(34)+chr(34)+chr(34)).replace(chr(120),chr(39)+a+chr(39)))
+a = """x"""
+b="""a = ""\"x""\"""";print(b);j='b=y';a='print(b);j=z;a=x;print(j.replace(chr(121),chr(34)*3+b.replace(chr(34)*3,chr(34)*2+chr(92)+chr(34))+chr(34)*3)+chr(59)+a.replace(chr(122),chr(39)+j+chr(39)).replace(chr(120),chr(39)+a+chr(39)))';print(j.replace(chr(121),chr(34)*3+b.replace(chr(34)*3,chr(34)*2+chr(92)+chr(34))+chr(34)*3)+chr(59)+a.replace(chr(122),chr(39)+j+chr(39)).replace(chr(120),chr(39)+a+chr(39)))
